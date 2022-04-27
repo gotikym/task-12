73b2d4a2d1fefc4b7548b2bae3c43c1c8c1634f4
One <One@DESKTOP-MIB22JC>
2022-04-27 22:56:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        int number = rand.Next(60, 101);
+        int sum = 0;
+
+        for (int i = number; i > 0; i--)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                sum = sum + i;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
