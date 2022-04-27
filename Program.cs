17c50fe2aa1993cf0c8b168f2287d1ee99eba9e5
Ivan Malkovskii <103892616+gotikym@users.noreply.github.com>
2022-04-27 23:22:22 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿using System;
+using System;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Random rand = new Random();
-        int number = rand.Next(60, 101);
+        Random random = new Random();
+        int number = random.Next(0, 101);
+        int dividerThree = 3, dividerFive = 5 ;
         int sum = 0;
 
         for (int i = number; i > 0; i--)
         {
-            if (i % 3 == 0 || i % 5 == 0)
+            if (i % dividerThree == 0 || i % dividerFive == 0)
             {
                 sum = sum + i;
                 Console.WriteLine(sum);
